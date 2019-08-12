3b7322c691b8252657d7b3969f3df7b95101e9b4
OmkoBass <omkob@hotmail.com>
2019-08-12 20:43:23 +0200

    MojBroj-MyNumber

---
+++ mojBroj/mojBroj/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace mojBroj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.brojPogadjas = new System.Windows.Forms.TextBox();
+            this.prvi = new System.Windows.Forms.TextBox();
+            this.drugi = new System.Windows.Forms.TextBox();
+            this.treci = new System.Windows.Forms.TextBox();
+            this.cetvrti = new System.Windows.Forms.TextBox();
+            this.peti = new System.Windows.Forms.TextBox();
+            this.sesti = new System.Windows.Forms.TextBox();
+            this.stop = new System.Windows.Forms.Button();
+            this.input = new System.Windows.Forms.TextBox();
+            this.ok = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.puta = new System.Windows.Forms.Button();
+            this.podeljeno = new System.Windows.Forms.Button();
+            this.levaZagrada = new System.Windows.Forms.Button();
+            this.desnaZagrada = new System.Windows.Forms.Button();
+            this.nazad = new System.Windows.Forms.Button();
+            this.trenutniBroj = new System.Windows.Forms.TextBox();
+            this.tr = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // brojPogadjas
+            // 
+            this.brojPogadjas.Font = new System.Drawing.Font("Fira Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.brojPogadjas.ForeColor = System.Drawing.SystemColors.Window;
+            this.brojPogadjas.Location = new System.Drawing.Point(146, 13);
+            this.brojPogadjas.Multiline = true;
+            this.brojPogadjas.Name = "brojPogadjas";
+            this.brojPogadjas.ReadOnly = true;
+            this.brojPogadjas.Size = new System.Drawing.Size(122, 38);
+            this.brojPogadjas.TabIndex = 0;
+            this.brojPogadjas.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // prvi
+            // 
+            this.prvi.Font = new System.Drawing.Font("Fira Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.prvi.ForeColor = System.Drawing.SystemColors.Window;
+            this.prvi.Location = new System.Drawing.Point(14, 68);
+            this.prvi.Multiline = true;
+            this.prvi.Name = "prvi";
+            this.prvi.ReadOnly = true;
+            this.prvi.Size = new System.Drawing.Size(38, 38);
+            this.prvi.TabIndex = 1;
+            this.prvi.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.prvi.Click += new System.EventHandler(this.Prvi_Click);
+            // 
+            // drugi
+            // 
+            this.drugi.Font = new System.Drawing.Font("Fira Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.drugi.ForeColor = System.Drawing.SystemColors.Window;
+            this.drugi.Location = new System.Drawing.Point(58, 68);
+            this.drugi.Multiline = true;
+            this.drugi.Name = "drugi";
+            this.drugi.ReadOnly = true;
+            this.drugi.Size = new System.Drawing.Size(38, 38);
+            this.drugi.TabIndex = 2;
+            this.drugi.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.drugi.Click += new System.EventHandler(this.Drugi_Click);
+            // 
+            // treci
+            // 
+            this.treci.Font = new System.Drawing.Font("Fira Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treci.ForeColor = System.Drawing.SystemColors.Window;
+            this.treci.Location = new System.Drawing.Point(102, 68);
+            this.treci.Multiline = true;
+            this.treci.Name = "treci";
+            this.treci.ReadOnly = true;
+            this.treci.Size = new System.Drawing.Size(38, 38);
+            this.treci.TabIndex = 3;
+            this.treci.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.treci.Click += new System.EventHandler(this.Treci_Click);
+            // 
+            // cetvrti
+            // 
+            this.cetvrti.Font = new System.Drawing.Font("Fira Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cetvrti.ForeColor = System.Drawing.SystemColors.Window;
+            this.cetvrti.Location = new System.Drawing.Point(146, 68);
+            this.cetvrti.Multiline = true;
+            this.cetvrti.Name = "cetvrti";
+            this.cetvrti.ReadOnly = true;
+            this.cetvrti.Size = new System.Drawing.Size(38, 38);
+            this.cetvrti.TabIndex = 4;
+            this.cetvrti.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.cetvrti.Click += new System.EventHandler(this.Cetvrti_Click);
+            // 
+            // peti
+            // 
+            this.peti.Font = new System.Drawing.Font("Fira Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.peti.ForeColor = System.Drawing.SystemColors.Window;
+            this.peti.Location = new System.Drawing.Point(204, 68);
+            this.peti.Multiline = true;
+            this.peti.Name = "peti";
+            this.peti.ReadOnly = true;
+            this.peti.Size = new System.Drawing.Size(72, 38);
+            this.peti.TabIndex = 5;
+            this.peti.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.peti.Click += new System.EventHandler(this.Peti_Click);
+            // 
+            // sesti
+            // 
+            this.sesti.Font = new System.Drawing.Font("Fira Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sesti.ForeColor = System.Drawing.SystemColors.Window;
+            this.sesti.Location = new System.Drawing.Point(297, 68);
+            this.sesti.Multiline = true;
+            this.sesti.Name = "sesti";
+            this.sesti.ReadOnly = true;
+            this.sesti.Size = new System.Drawing.Size(91, 38);
+            this.sesti.TabIndex = 6;
+            this.sesti.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.sesti.Click += new System.EventHandler(this.Sesti_Click);
+            // 
+            // stop
+            // 
+            this.stop.Font = new System.Drawing.Font("Fira Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stop.Location = new System.Drawing.Point(170, 112);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(56, 31);
+            this.stop.TabIndex = 7;
+            this.stop.Text = "STOP";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // input
+            // 
+            this.input.Font = new System.Drawing.Font("Fira Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input.Location = new System.Drawing.Point(14, 167);
+            this.input.Multiline = true;
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(374, 38);
+            this.input.TabIndex = 8;
+            this.input.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.input.TextChanged += new System.EventHandler(this.Input_TextChanged);
+            // 
+            // ok
+            // 
+            this.ok.Font = new System.Drawing.Font("Fira Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ok.Location = new System.Drawing.Point(170, 211);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(56, 31);
+            this.ok.TabIndex = 9;
+            this.ok.Text = "OK";
+            this.ok.UseVisualStyleBackColor = true;
+            this.ok.Click += new System.EventHandler(this.Ok_Click);
+            // 
+            // plus
+            // 
+            this.plus.Font = new System.Drawing.Font("Fira Code", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plus.Location = new System.Drawing.Point(472, 8);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(43, 33);
+            this.plus.TabIndex = 10;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.Plus_Click);
+            // 
+            // minus
+            // 
+            this.minus.Font = new System.Drawing.Font("Fira Code", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minus.Location = new System.Drawing.Point(472, 47);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(43, 33);
+            this.minus.TabIndex = 11;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.Minus_Click);
+            // 
+            // puta
+            // 
+            this.puta.Font = new System.Drawing.Font("Fira Code", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.puta.Location = new System.Drawing.Point(472, 87);
+            this.puta.Name = "puta";
+            this.puta.Size = new System.Drawing.Size(43, 33);
+            this.puta.TabIndex = 12;
+            this.puta.Text = "*";
+            this.puta.UseVisualStyleBackColor = true;
+            this.puta.Click += new System.EventHandler(this.Puta_Click);
+            // 
+            // podeljeno
+            // 
+            this.podeljeno.Font = new System.Drawing.Font("Fira Code", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.podeljeno.Location = new System.Drawing.Point(472, 125);
+            this.podeljeno.Name = "podeljeno";
+            this.podeljeno.Size = new System.Drawing.Size(43, 33);
+            this.podeljeno.TabIndex = 13;
+            this.podeljeno.Text = "÷";
+            this.podeljeno.UseVisualStyleBackColor = true;
+            this.podeljeno.Click += new System.EventHandler(this.Podeljeno_Click);
+            // 
+            // levaZagrada
+            // 
+            this.levaZagrada.Font = new System.Drawing.Font("Fira Code", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.levaZagrada.Location = new System.Drawing.Point(472, 164);
+            this.levaZagrada.Name = "levaZagrada";
+            this.levaZagrada.Size = new System.Drawing.Size(43, 33);
+            this.levaZagrada.TabIndex = 14;
+            this.levaZagrada.Text = "(";
+            this.levaZagrada.UseVisualStyleBackColor = true;
+            this.levaZagrada.Click += new System.EventHandler(this.LevaZagrada_Click);
+            // 
+            // desnaZagrada
+            // 
+            this.desnaZagrada.Font = new System.Drawing.Font("Fira Code", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.desnaZagrada.Location = new System.Drawing.Point(472, 203);
+            this.desnaZagrada.Name = "desnaZagrada";
+            this.desnaZagrada.Size = new System.Drawing.Size(43, 33);
+            this.desnaZagrada.TabIndex = 15;
+            this.desnaZagrada.Text = ")";
+            this.desnaZagrada.UseVisualStyleBackColor = true;
+            this.desnaZagrada.Click += new System.EventHandler(this.DesnaZagrada_Click);
+            // 
+            // nazad
+            // 
+            this.nazad.Font = new System.Drawing.Font("Fira Code", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nazad.Location = new System.Drawing.Point(394, 167);
+            this.nazad.Name = "nazad";
+            this.nazad.Size = new System.Drawing.Size(72, 38);
+            this.nazad.TabIndex = 16;
+            this.nazad.Text = "<---";
+            this.nazad.UseVisualStyleBackColor = true;
+            this.nazad.Click += new System.EventHandler(this.Nazad_Click);
+            // 
+            // trenutniBroj
+            // 
+            this.trenutniBroj.Font = new System.Drawing.Font("Fira Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trenutniBroj.Location = new System.Drawing.Point(394, 13);
+            this.trenutniBroj.Multiline = true;
+            this.trenutniBroj.Name = "trenutniBroj";
+            this.trenutniBroj.ReadOnly = true;
+            this.trenutniBroj.Size = new System.Drawing.Size(72, 38);
+            this.trenutniBroj.TabIndex = 17;
+            this.trenutniBroj.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tr
+            // 
+            this.tr.AutoSize = true;
+            this.tr.Font = new System.Drawing.Font("Fira Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tr.Location = new System.Drawing.Point(306, 20);
+            this.tr.Name = "tr";
+            this.tr.Size = new System.Drawing.Size(82, 21);
+            this.tr.TabIndex = 18;
+            this.tr.Text = "TREUNTNO:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(525, 258);
+            this.Controls.Add(this.tr);
+            this.Controls.Add(this.trenutniBroj);
+            this.Controls.Add(this.nazad);
+            this.Controls.Add(this.desnaZagrada);
+            this.Controls.Add(this.levaZagrada);
+            this.Controls.Add(this.podeljeno);
+            this.Controls.Add(this.puta);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.ok);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.sesti);
+            this.Controls.Add(this.peti);
+            this.Controls.Add(this.cetvrti);
+            this.Controls.Add(this.treci);
+            this.Controls.Add(this.drugi);
+            this.Controls.Add(this.prvi);
+            this.Controls.Add(this.brojPogadjas);
+            this.Name = "Form1";
+            this.Text = "Moj broj";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox brojPogadjas;
+        private System.Windows.Forms.TextBox prvi;
+        private System.Windows.Forms.TextBox drugi;
+        private System.Windows.Forms.TextBox treci;
+        private System.Windows.Forms.TextBox cetvrti;
+        private System.Windows.Forms.TextBox peti;
+        private System.Windows.Forms.TextBox sesti;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button ok;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button puta;
+        private System.Windows.Forms.Button podeljeno;
+        private System.Windows.Forms.Button levaZagrada;
+        private System.Windows.Forms.Button desnaZagrada;
+        private System.Windows.Forms.Button nazad;
+        private System.Windows.Forms.TextBox trenutniBroj;
+        private System.Windows.Forms.Label tr;
+    }
+}
+
---
+++ mojBroj/mojBroj/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Collections;
+
+namespace mojBroj
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+            input.ReadOnly = true;
+            ok.Enabled = false;
+            plus.Enabled = false;
+            minus.Enabled = false;
+            puta.Enabled = false;
+            podeljeno.Enabled = false;
+            levaZagrada.Enabled = false;
+            desnaZagrada.Enabled = false;
+
+        }
+
+        int brojac = 0;
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            //Simulira stop, (maybe stupid)
+            int[] brojevi = { 10, 15, 25 };
+            int[] drugiBrojevi = { 50, 75, 100 };
+            Random r = new Random();
+
+            if (brojac == 0) { brojPogadjas.Text = r.Next(1, 1000).ToString(); }
+            else if (brojac == 1) { prvi.Text = r.Next(1, 10).ToString(); }
+            else if (brojac == 2) { drugi.Text = r.Next(1, 10).ToString(); }
+            else if (brojac == 3) { treci.Text = r.Next(1, 10).ToString(); }
+            else if (brojac == 4) { cetvrti.Text = r.Next(1, 10).ToString(); }
+            else if (brojac == 5) { peti.Text = brojevi[r.Next(brojevi.Length)].ToString(); }
+            else if (brojac == 6)
+            {
+                //Setamo zadnji broj i postavljamo da mozemo da radimo sa dugmicima
+                sesti.Text = drugiBrojevi[r.Next(drugiBrojevi.Length)].ToString();
+                ok.Enabled = true;
+                prvi.BackColor = Color.DarkRed;
+                drugi.BackColor = Color.DarkRed;
+                treci.BackColor = Color.DarkRed;
+                cetvrti.BackColor = Color.DarkRed;
+                peti.BackColor = Color.DarkRed;
+                sesti.BackColor = Color.DarkRed;
+                plus.Enabled = true;
+                minus.Enabled = true;
+                puta.Enabled = true;
+                podeljeno.Enabled = true;
+                levaZagrada.Enabled = true;
+                desnaZagrada.Enabled = true;
+            }
+            brojac++;
+        }
+
+        private void Ok_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(input.Text) == false)
+            {
+                string s = MathParser.EvalExpression(input.Text.ToCharArray()).ToString();
+                int x = Int16.Parse(s);
+                int y = Int16.Parse(brojPogadjas.Text);
+
+                if (x == y) { MessageBox.Show("Tacno!"); this.Close(); }
+                else { MessageBox.Show($"Netacno! Tvoj broj je {x}"); this.Close(); }
+            }
+            else
+            {
+                MessageBox.Show("Los unos!"); this.Close();
+            }
+        }
+
+        private void Plus_Click(object sender, EventArgs e)
+        {
+            input.AppendText("+");
+        }
+
+        private void Minus_Click(object sender, EventArgs e)
+        {
+            input.AppendText("-");
+        }
+
+        private void Puta_Click(object sender, EventArgs e)
+        {
+            input.AppendText("*");
+        }
+
+        private void Podeljeno_Click(object sender, EventArgs e)
+        {
+            input.AppendText("÷");
+        }
+
+        private void Prvi_Click(object sender, EventArgs e)
+        {
+            input.AppendText(prvi.Text);
+            prvi.BackColor = Color.White;
+            prvi.Enabled = false;
+        }
+
+        private void Drugi_Click(object sender, EventArgs e)
+        {
+            input.AppendText(drugi.Text);
+            drugi.BackColor = Color.White;
+            drugi.Enabled = false;
+        }
+
+        private void Treci_Click(object sender, EventArgs e)
+        {
+            input.AppendText(treci.Text);
+            treci.BackColor = Color.White;
+            treci.Enabled = false;
+        }
+
+        private void Cetvrti_Click(object sender, EventArgs e)
+        {
+            input.AppendText(cetvrti.Text);
+            cetvrti.BackColor = Color.White;
+            cetvrti.Enabled = false;
+        }
+
+        private void Peti_Click(object sender, EventArgs e)
+        {
+            input.AppendText(peti.Text);
+            peti.BackColor = Color.White;
+            peti.Enabled = false;
+        }
+
+        private void Sesti_Click(object sender, EventArgs e)
+        {
+            input.AppendText(sesti.Text);
+            sesti.BackColor = Color.White;
+            sesti.Enabled = false;
+        }
+
+        private void LevaZagrada_Click(object sender, EventArgs e)
+        {
+            input.AppendText("(");
+        }
+
+        private void DesnaZagrada_Click(object sender, EventArgs e)
+        {
+            input.AppendText(")");
+        }
+
+        private void Nazad_Click(object sender, EventArgs e)
+        {
+            input.Text = "";
+            prvi.BackColor = Color.DarkRed;
+            drugi.BackColor = Color.DarkRed;
+            treci.BackColor = Color.DarkRed;
+            cetvrti.BackColor = Color.DarkRed;
+            peti.BackColor = Color.DarkRed;
+            sesti.BackColor = Color.DarkRed;
+            prvi.Enabled = true;
+            drugi.Enabled = true;
+            treci.Enabled = true;
+            cetvrti.Enabled = true;
+            peti.Enabled = true;
+            sesti.Enabled = true;
+        }
+
+        private void Input_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string s = MathParser.EvalExpression(input.Text.ToCharArray()).ToString();
+                trenutniBroj.Text = s;
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
